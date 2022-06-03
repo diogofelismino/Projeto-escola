8bfb0153c6a72e3d1fa5910e16086e1d8e7fc0dd
Diogo Felismino da Silva <diogo.silva@notacontrol.matriz>
2022-06-03 16:00:11 -0400

    Criando tela de cadastrato e de recupera senha e cirando envento de botao

---
+++ muda.js
@@ -0,0 +1,24 @@
+
+
+function Teste1(evt){
+    evt.preventDefault()
+    let modal = document.querySelector('.telaSenha')
+    modal.style.visibility = "visible"
+    let modal2 = document.querySelector('.telaLogin')
+    modal2.style.visibility = "hidden"
+    console.log("asdasdasd")
+    
+}
+
+function Teste(evt){
+    let modal = document.querySelector('.telaSenha')
+    modal.style.visibility = "hidden"
+    let modal2 = document.querySelector('.telaLogin')
+    modal2.style.visibility = "visible"
+    console.log("asdasdasd")
+    
+}
+
+
+
+
